fc65320eaa8aecf79c4d27b0a656a7b1c7ed9998
yuukou3333 <kuusou3333@gmail.com>
2020-01-22 15:36:29 +0900

    Q1~4の回答

--- task.rb
+++ task.rb
@@ -5,7 +5,8 @@ def q1
   names = ["田中", "佐藤", "佐々木", "高橋"]
 
   # 以下に回答を記載
-
+  names[0] = "斎藤"
+  p names[0]
 end
 
 def q2
@@ -13,6 +14,9 @@ def q2
   array2 = %w(bird bat tiger)
 
   # 以下に回答を記載
+  array = array1 + array2
+  p array
+  
 
 end
 
@@ -20,13 +24,15 @@ def q3
   numbers = [1, 5, 8, 10, 2, 3, 2, 3, 3, 1, 4, 5, 9]
 
   # 以下に回答を記載
-
+  p numbers.count(3)
 end
 
 def q4
   sports = ["サッカー", "フットサル", nil, "野球", "バスケ", nil, "バレー"]
 
   # 以下に回答を記載
+  sports.delete(nil)
+  p sports
 
 end
 
